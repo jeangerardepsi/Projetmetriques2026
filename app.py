717c823222ab267a843df19e437c4718919a2fec
JG <jeangerardhounkanrin@gmail.com>
2026-04-02 16:56:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-import requests
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+# Métriques : Températures à Paris
+data_paris = [
+    {"heure": "08:00", "temp": 12},
+    {"heure": "10:00", "temp": 14},
+    {"heure": "12:00", "temp": 18},
+    {"heure": "14:00", "temp": 20},
+    {"heure": "16:00", "temp": 19},
+    {"heure": "18:00", "temp": 15}
+]
+
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
+def index():
+    # Envoie vers le fichier HTML dans le dossier templates
+    return render_template('graphique.html')
 
-# Déposez votre code à partir d'ici :
+@app.route('/api/paris')
+def api_paris():
+    # Envoie les données au format JSON pour le graphique
+    return jsonify(data_paris)
 
-
-
-
-
-
-# Ne rien mettre après ce commentaire
-    
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
